c99eb17850e51327449e6fac5572c6cfc436d21c
Paul Bonaud <paul.bonaud@fretlink.com>
2020-03-24 10:18:14 +0100

    vars: fix `clever_base_env`  by removing a variable if not necessary
    The `CC_RUN_COMMAND` variable was defined in case we use haskell
    binary as entrypoints. However if the `clever_haskell_entry_point`
    variable is not defined we shouldn't define the `CC_RUN_COMMAND` env variable.

---
+++ tests/fake.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+binary="${0/*\/}"
+fakeCommit="aaa000aaa000aaa000aaa000aaa000aaa000aaa0"
+
+if [ "${binary}" = "clever" ] && [ "${1}" = "--version" ]; then
+    echo "1.6.3"
+elif [ "${binary}" = "clever" ] && [ "${1}" = "activity" ]; then
+    echo "2020-02-02T20:20:02+02:00  OK         DEPLOY     ${fakeCommit}  Git"
+elif [ "${binary}" = "git" ]; then
+    echo "${fakeCommit}"
+else
+    echo "${binary} called with arguments: ${*}"
+fi
